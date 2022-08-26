3e5986992d5de49cee9e156ed588d7b7b13707df
Ryad Chair <r.chair@hotmail.fr>
2022-08-26 15:26:18 +0200

    Add Logo and Navigation components

---
+++ src/containers/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Navigation.css';
+
+const Navigation = () => {
+  return (
+    <nav className="nav">
+      <p className="nav__link">Sign out</p>
+    </nav>
+  )
+};
+
+export default Navigation;
